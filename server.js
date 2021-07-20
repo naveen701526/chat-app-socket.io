86d24440eb01f9ba2e6d480fb0fad54da6585aa5
naveen <neonew8984@gmail.com>
2021-07-20 15:58:18 +0530

    inform user when they login or someone left the chat

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const formatMessage = require('./utils/messages');
 const app = express();
 
 const server = http.createServer(app);
@@ -11,20 +12,25 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'ChatCord Bot';
+
 // run when client connects
 io.on('connection', (socket) => {
   //   welcome the current user
-  socket.emit('message', 'Welcome to ChatCord!');
+  socket.emit('message', formatMessage(botName, 'Welcome To ChatCord!'));
   //   broadcast when a user connects
-  socket.broadcast.emit('message', 'A user has joined the chat');
+  socket.broadcast.emit(
+    'message',
+    formatMessage(botName, 'A user has joined the chat')
+  );
   //   runs when client disconnects
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left the chat');
+    io.emit('message', formatMessage(botName, 'A user has left the chat'));
   });
 
   //   listen for chatMessage
   socket.on('chatMessage', (msg) => {
-    io.emit('message', msg);
+    io.emit('message', formatMessage('USER', msg));
   });
 });
 
