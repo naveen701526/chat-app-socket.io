026220779856654ed72dad00a33cfef2d23c4168
naveen <neonew8984@gmail.com>
2021-07-20 15:32:30 +0530

    sending message from client to server and vice versa

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
 const app = express();
 
+const server = http.createServer(app);
+const socketio = require('socket.io');
+
+const io = socketio(server);
 // set static folder
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// run when client connects
+io.on('connection', (socket) => {
+  //   welcome the current user
+  socket.emit('message', 'Welcome to ChatCord!');
+  //   broadcast when a user connects
+  socket.broadcast.emit('message', 'A user has joined the chat');
+  //   runs when client disconnects
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat');
+  });
+
+  //   listen for chatMessage
+  socket.on('chatMessage', (msg) => {
+    console.log(msg);
+  });
+});
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server Running on PORT: ${PORT}`));
+server.listen(PORT, () => console.log(`Server Running on PORT: ${PORT}`));
